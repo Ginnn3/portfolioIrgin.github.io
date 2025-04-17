ad27fdcfbbf3928f8100836a18685a2041597016
Ginnn <134420600+Ginnn3@users.noreply.github.com>
2025-04-17 17:36:44 +0700

    Delete app directory

--- app/js/link-active.js
+++
@@ -1,9 +0,0 @@
-// Nav active
-const navLinkEls = document.querySelectorAll(".nav-link");
-
-navLinkEls.forEach((navLinkEl) => {
-  navLinkEl.addEventListener("click", () => {
-    document.querySelector(".active")?.classList.remove("active");
-    navLinkEl.classList.add("active");
-  });
-});
